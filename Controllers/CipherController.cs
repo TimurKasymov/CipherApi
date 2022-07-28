aebeab90e69271ad8ffdc06f7a7a42edc5ead98d
Tim45363 <kasymovty@gmail.com>
2022-07-28 17:12:44 +0300

    done

--- Controllers/CipherController.cs
+++ Controllers/CipherController.cs
@@ -19,14 +19,18 @@ namespace cipher.Controllers
               [HttpGet("encrypt")]
               public async Task<IActionResult> EncryptString(string value)
               {
+                     _logger.LogInformation("Encription started..");
                      var result = await CipherManager.EncryptStringAsync(value, _cipherSettings.Value.Key);
+                     _logger.LogInformation("Encription finished..");
                      return Ok(result);
               }
 
               [HttpGet("decrypt")]
               public async Task<IActionResult> DecryptString(string value)
               {
+                     _logger.LogInformation("Decryption started..");
                      var result = await CipherManager.DecryptAsync(value, _cipherSettings.Value.Key);
+                     _logger.LogInformation("Decryption finished..");
                      return Ok(result);
               }
        }
